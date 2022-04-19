e8a64f2b72b62fa8c39039e83e7ea26971eb0948
egiam <ezegiampaoli@gmail.com>
2022-04-19 17:05:36 -0300

    Vid 6

--- 1 Start-Coding/1 JavaScript/1 Basics/script.js
+++ 1 Start-Coding/1 JavaScript/1 Basics/script.js
@@ -2,3 +2,28 @@
 //alert("Hello world");
 
 //TODO: Video 6
+//Variables
+
+//Number
+console.log(3); //typeof dice el tipo de variable
+console.log(3.5);
+console.log(Infinity);
+console.log(NaN);
+
+//String
+console.log("Hello, World");
+console.log(typeof "Hello");
+console.log("3");
+
+//Bool
+console.log(true);
+console.log(false);
+
+//Null y Undefined
+console.log(undefined);
+console.log(null);
+
+let myVariable = null;
+console.log(myVariable);
+
+//
