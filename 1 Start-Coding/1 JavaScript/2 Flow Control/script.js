d310e3630543a24b6074aaba539ce74162fdbd62
egiam <ezegiampaoli@gmail.com>
2022-04-20 20:47:39 -0300

    Vid 1 & 2

---
+++ 1 Start-Coding/1 JavaScript/2 Flow Control/script.js
@@ -0,0 +1,49 @@
+//TODO: Vid 1
+
+//Control de flujo
+
+//if / else
+
+let age = 17;
+
+if (age >= 18) {
+    console.log("Podes manejar");
+} else {
+    console.log("No podes manejar");
+}
+
+let number = 10;
+
+if (number < 10) {
+    console.log("El numero es menor a 10.");
+} else if (number > 10) {
+    console.log("El numero es mayor a 10.");
+} else if (number === 10) {
+    console.log("El numero es 10.");
+} else {
+    console.log("No es un numero.");
+}
+
+//switch
+
+const weather = "fresco";
+
+switch (weather.toUpperCase()) {
+    case "FRESCO":
+        console.log("Lleva Abrigo");
+        break;
+
+    case "LLUVIOSO":
+        console.log("Lleva Paraguas");
+        break;
+
+    case "SOLEADO":
+        console.log("Cuidate del sol");
+        break;
+
+    default:
+        console.log("El tiempo esta bueno. Disfruta del dia :)");
+        break;
+}
+
+//
