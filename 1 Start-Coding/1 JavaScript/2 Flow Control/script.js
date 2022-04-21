ff7526f0c53de796e156f4d45a2dfea58bc0f40d
egiam <ezegiampaoli@gmail.com>
2022-04-20 21:48:39 -0300

    Excercises

--- 1 Start-Coding/1 JavaScript/2 Flow Control/script.js
+++ 1 Start-Coding/1 JavaScript/2 Flow Control/script.js
@@ -72,4 +72,86 @@ while (number < 1) {
     number++;
 }
 
+//Ejercicios
+//1
+
+let variable = "#";
+
+for (let i = 0; i <= 4; i++) {
+    console.log(variable);
+    variable += "#";
+}
+
+console.log("#");
+console.log("##");
+console.log("###");
+console.log("####");
+console.log("#####");
+
+/* 1) Triangulo de #. Para este ejercicio, construir un programa que una vez concluido nos permita ver en consola un triangulo
+formado con #, de la siguiente manera:
+#
+##
+###
+####
+#####
+ 
+(PISTA: Recuerden que yo puedo agarrar una variable, tomar su valor y concatenarla con otra cosa asi
+=> miVariable = miVariable + 'algoMas")
+*/
+
+let output = "";
+
+for (let i = 1; i <= 5; i++) {
+    output += "#";
+    console.log(output);
+}
+
+/*2) Numeros pares e impares. Hacer un programa que itere por los numeros del 0 al 10 y, por cada uno de ellos, imprima el numero y
+nos diga si es par o impar. 
+Ejemplo:
+0 es par
+1 es impar
+2 es par
+...
+*/
+
+for (let i = 0; i <= 10; i++) {
+    if (i % 2 === 0) console.log(`${i} es par`);
+    else console.log(`${i} es impar`);
+}
+
+/*3) FizzBuzz. Generar un programa que itere del 1 al 15. Por cada iteracion, mostrar por consola lo siguiente:
+1- Si el numero es multimo de 3, mostrar la palabra "Fizz"
+2- Si el numero es multiplo de 5, mostrar la palabra "Buzz"
+3- Si el numero es multiplo de 3 y 5, mostrar la palabra "FizzBuzz"
+4- En los demas casos, mostrar el numero
+*/
+
+for (let i = 1; i <= 15; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+
+    let text = "";
+
+    if (i % 3 === 0) {
+        text += "Fizz";
+    }
+
+    if (i % 5 === 0) {
+        text += "Buzz";
+    } else if (i % 3 !== 0 && i % 5 !== 0) {
+        text = i;
+    }
+
+    console.log(text);
+}
+
 //
