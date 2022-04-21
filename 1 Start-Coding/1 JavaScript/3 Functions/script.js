59e4ad69a1655aaecf1d4f2dce8139647ecf23f5
egiam <ezegiampaoli@gmail.com>
2022-04-21 20:34:57 -0300

    Vid 2

---
+++ 1 Start-Coding/1 JavaScript/3 Functions/script.js
@@ -0,0 +1,24 @@
+//Vid 1
+
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+
+sum(2, 3);
+sum(3, 13);
+sum(3, 43);
+
+//Vid 2
+
+function sayHello() {
+    return "Hola Mundo";
+}
+
+console.log(sayHello());
+console.log(sum(2, 3) + 5);
+
+const results = (num1, num2) => num1 + num2;
+
+console.log(results(2, 3) + 5);
+
+//
