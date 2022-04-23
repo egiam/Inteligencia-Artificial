01164ec180350bea95d150eda0ba1f036622402d
egiam <ezegiampaoli@gmail.com>
2022-04-22 21:02:28 -0300

    Vid 1

---
+++ 1 Start-Coding/1 JavaScript/4 Arrays/script.js
@@ -0,0 +1,17 @@
+//TODO: Modulo 4 Arrays
+
+// Vid 1
+
+const miArray = ["La mochila de Alexis", "Cartera de Maria", "Bolsa de Juan"];
+// const otroArray = new Array();
+
+const otroArray = [
+    ["Juan", 30, {}], "Alexis", 2, true
+];
+console.log(otroArray);
+console.log(otroArray[2]);
+
+const elemento = otroArray[0];
+console.log(elemento);
+
+//
