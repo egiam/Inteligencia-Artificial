1692d6859ec6e0902c57aa0f03d7cfd19c7b483b
egiam <ezegiampaoli@gmail.com>
2022-04-22 22:11:06 -0300

    Vid 4

--- 1 Start-Coding/1 JavaScript/4 Arrays/script.js
+++ 1 Start-Coding/1 JavaScript/4 Arrays/script.js
@@ -14,7 +14,15 @@ console.log(otroArray[2]);
 const elemento = otroArray[0];
 console.log(elemento);
 
-const nombres = ["Alexis", "juan", "Pedro", "Marcelo"];
+const nombres = [
+    "Alexis",
+    "juan",
+    "Pedro",
+    "Marcelo",
+    "Armando",
+    "Jorge",
+    "Emanuel",
+];
 
 console.log(nombres[nombres.length - 1]); //Para acceder al ultimo elemento del array.
 const lastElement = nombres[nombres.length - 1];
@@ -29,5 +37,15 @@ nombres.unshift("Jorge"); //Agregar un elemento al comienzo del Array
 nombres.shift(); //Eliminar un elemento del comienzo del Array
 nombres.splice(1, 3); //Remover desde la posicion [1] hasta la posicion [3] (Remueve 1,2,3)
 const newNombres = nombres.slice(1, 4); //Copiar al otro array solo desde la posicion [1] hasta la posicion [4]
+
+//Vid 4
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
+
+nombres.forEach((item) => {
+    console.log(item);
+});
 
 //
