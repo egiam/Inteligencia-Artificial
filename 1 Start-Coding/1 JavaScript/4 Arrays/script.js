da3258c1d4a8f5075a182b0ff7cc638a77768af0
egiam <ezegiampaoli@gmail.com>
2022-04-22 21:56:14 -0300

    Vid 3

--- 1 Start-Coding/1 JavaScript/4 Arrays/script.js
+++ 1 Start-Coding/1 JavaScript/4 Arrays/script.js
@@ -22,4 +22,12 @@ const lastElement = nombres[nombres.length - 1];
 const element = "Pedro";
 const index = nombres.indexOf(element); //Da el indice en el array de ese elemento.
 
+//Vid 3
+nombres.push("Jorge"); //Agregar elemento
+nombres.pop(); //Elimina el ultimo elemento
+nombres.unshift("Jorge"); //Agregar un elemento al comienzo del Array
+nombres.shift(); //Eliminar un elemento del comienzo del Array
+nombres.splice(1, 3); //Remover desde la posicion [1] hasta la posicion [3] (Remueve 1,2,3)
+const newNombres = nombres.slice(1, 4); //Copiar al otro array solo desde la posicion [1] hasta la posicion [4]
+
 //
