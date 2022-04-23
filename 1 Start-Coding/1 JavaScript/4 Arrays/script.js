e52508224008d947cbb995e5a0a0c3067910e450
egiam <ezegiampaoli@gmail.com>
2022-04-22 21:07:44 -0300

    Vid 2

--- 1 Start-Coding/1 JavaScript/4 Arrays/script.js
+++ 1 Start-Coding/1 JavaScript/4 Arrays/script.js
@@ -14,4 +14,12 @@ console.log(otroArray[2]);
 const elemento = otroArray[0];
 console.log(elemento);
 
+const nombres = ["Alexis", "juan", "Pedro", "Marcelo"];
+
+console.log(nombres[nombres.length - 1]); //Para acceder al ultimo elemento del array.
+const lastElement = nombres[nombres.length - 1];
+
+const element = "Pedro";
+const index = nombres.indexOf(element); //Da el indice en el array de ese elemento.
+
 //
