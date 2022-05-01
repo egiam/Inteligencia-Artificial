8a68fcb80034b096119bbf129f04f76919a2f7c9
egiam <ezegiampaoli@gmail.com>
2022-04-30 23:39:52 -0300

    Update script.js

--- 1 Start-Coding/1 JavaScript/6 Asynchrony/script.js
+++ 1 Start-Coding/1 JavaScript/6 Asynchrony/script.js
@@ -156,4 +156,198 @@ const getDataFromApi = async() => {
 
 console.log(getDataFromApi().then(response => console.log(response)));
 
+
+
+//ASINCRONISMO Y CALL STACK - ROUND 2
+
+//EJEMPLO 1
+
+const asyncOperation = new Promise((resolve) => resolve());
+
+asyncOperation.then((resolvedValue) => console.log("Mario"));
+console.log("Juan");
+console.log("Pedro");
+
+//EJEMPLO 2
+const asyncOperation2 = fetch("https://jsonplaceholder.typicode.com/todos/1"); //Fetch es una promesa tambien
+asyncOperation2.then((resolvedValue) => console.log("Mario"));
+console.log("Juan");
+console.log("Pedro");
+
+//EJEMPLO 3
+async function fetchSomething() {
+    await new Promise((resolve) => { //Tiene await, esperamos a q se resuelva. Pausa la ejecucion de codigo, espera a q la promesa se resuelva y despues sigue ejecutando el codigo
+        setTimeout(() => {
+            console.log("Mario");
+            resolve();
+        }, 10);
+    });
+
+    console.log("Juan"); //Retorna al ultimo, xq hace esperar a q funciones la promise de mario para poder usarse
+}
+
+fetchSomething();
+
+console.log("Pedro"); //Es codigo sincronico x estar fuera de la funcion async
+
+///EJEMPLO 4
+setTimeout(() => console.log("Mario"), 10);
+
+const promise = new Promise((resolve) => { //La promesa tiene prioridad sobre el setTimeout
+    setTimeout(() => resolve(), 10);
+});
+
+promise.then(() => console.log("Juan")); //.then no detiene la ejecucion de otras funcionalidades asincronicas.
+console.log("Pedro");
+
+//EJEMPLO 5
+
+setTimeout(() => console.log("Mario"), 0); //Mario ultimo, xq promesa tiene prioridad sobre setTimeout
+const promise1 = Promise.resolve();
+promise1.then(() => console.log("Juan")); //Juan con prioridad xq es promesa
+console.log("Pedro");
+
+// 1) Vamos a simular una carrera entre 3 personas, Juan,
+// Mario y Martin. El orden de largada sera siempre el
+//siguiente:
+
+// console.log("Juan");
+// console.log("Mario");
+// console.log("Martin");
+
+// El orden de llegada se determina conforme el
+// nombre de cada uno vaya apareciendo en la consola,
+// de modo que el ganador sera aquel cuyo nombre sea
+//el primero en aparecer.
+
+// ACTIVIDAD: Utilizando solamente setTimeout y sin
+// alterar el orden de lagrada, escribir el codigo
+// necesario para obtener los siguientes resultados:
+
+// Vuelta 1:
+// Mario
+// Martin
+// Juan
+
+setTimeout(() => console.log("Juan"), 0);
+console.log("Mario");
+console.log("Martin");
+
+//Vuelta 2:
+// Juan
+// Martin
+// Mario
+
+console.log("Juan");
+setTimeout(() => console.log("Mario"), 0);
+console.log("Martin");
+
+//Vuelta 3
+// Martin
+// Juan
+// Mario
+
+// setTimeout(() => console.log("Juan"), 0);
+// setTimeout(() => console.log("Mario"), 1);
+setTimeout(() => {
+    console.log("Juan");
+    console.log("Mario");
+}, 0);
+console.log("Martin");
+
+
+// Vuelta 4
+// Mario
+// Juan
+// Martin
+
+setTimeout(() => console.log("Juan"), 10);
+console.log("Mario");
+setTimeout(() => console.log("Martin"), 15);
+
+// 2) Utilizando setTimeout, crear una funcion que
+// actue como un temporizador, haciendo una cuenta
+// regresiva desde el numero de segundos que se pase
+// como argumentos hasta llegar a 0. Para ese ejercicio
+// no se puede utilizar ningun tipo de loop (for, while, etc)
+//PISTA: Para resolver el ejerciio, pueden investigar un
+// poco sobre funciones recursivas.
+
+await
+
+function temporizador(segundos) {
+    if (segundos < 0) return;
+    setTimeout(() => console.log(segundos), 1000);
+    temporizador(segundos - 1);
+}
+
+temporizador(17);
+
+//Profe
+
+const temporizador1 = (second) => {
+    setTimeout(() => {
+        if (second < 0) return;
+
+        console.log(second);
+
+        temporizador(second - 1);
+    }, 1000);
+};
+
+temporizador1(10);
+
+//3) Utilizando la API de GITHUB (https://api.github.com)
+//realizar las siguientes tareas
+
+// a) Obtener una lista de los repositorios publicos
+// (Documentacion de la API: https://docs.github.com/es/rest/reference/repos#list-public-repositories)
+
+const baseUrl = "https://api.github.com";
+
+const getPublicRepositories = async() => {
+    const response = await fetch(`${baseUrl}/repositories`);
+    const jsonResponse = await response.json();
+    //   console.log(jsonResponse);
+    return jsonResponse;
+};
+
+//getPublicRepositories();
+
+// b) Generar una funcion que a partir de la respuesta
+// del punto anterior, retorne un array de los nombres
+// de usuario de los owners de los repositorios listados
+
+const getOwnersNames = async() => {
+    const repositories = await getPublicRepositories();
+
+    const namesArray = repositories.map((repo) => repo.owner.login);
+
+    console.log(namesArray);
+};
+
+getOwnersNames();
+
+//c) Tomar de la respuesta al primer respositorio, y
+// listar todos los repositorios del owner.
+// PISTA: Para ver el endpoint al cual deberas pegarle
+// para traer los repositorios del owner, deberas
+// inspeccionar la informacion que recibes dentro del
+// primer request.
+
+const getRepositoriesFromOwner = async() => {
+    const repositories = await getPublicRepositories();
+    const firstRepository = repositories[0];
+
+    const reposEndpoint = firstRepository.owner.repos_url;
+
+    const reposResponse = await fetch(reposEndpoint);
+    const jsonReposResponse = await reposResponse.json();
+
+    console.log(jsonReposResponse);
+};
+
+getRepositoriesFromOwner();
+
+
 //
