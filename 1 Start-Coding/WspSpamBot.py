e0c0143c2c446975adfa929026a0d3ad948ab880
egiam <ezegiampaoli@gmail.com>
2022-05-02 15:57:08 -0300

    extra

---
+++ 1 Start-Coding/WspSpamBot.py
@@ -0,0 +1,9 @@
+# import pywhatkit
+# pywhatkit.sendwhatmsg('+5493517892619', 'You are special in my heart, sweetie. Forever will you, darling.', 1, 46)
+
+import pywhatkit
+ 
+for x in range(10):
+    pywhatkit.sendwhatmsg("+5493516190104",
+                          "Esto es una prueba",
+                          14, 25, 5)
