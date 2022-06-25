8ab580098b2911468e5a53c842d7313937e43c87
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-06-25 09:57:59 -0300

    Update

--- 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/detail/index.jsx
+++ 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/detail/index.jsx
@@ -1,10 +1,15 @@
+import { useParams } from "react-router-dom";
+
 export const DetailContainer = () => {
+  const { id } = useParams(); //Busca el parametro que se genera de ID en el URL y lo guarda en la variable id.
+
   return (
-    <div>
-      <h1>Nombre del producto</h1>
+    <>
+      URL
+      <h1>Nombre del producto - {id}</h1>
       <strong>Descripcion</strong>
       <a href="#">$9.999</a>
       <button>Comprar</button>
-    </div>
+    </>
   );
 };
--- 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/list/index.jsx
+++ 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/list/index.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const ListContainer = () => {
+  const list = [
+    { id: 1, name: "shoes", description: "this is a shoe", price: 999 },
+    { id: 2, name: "shirt", description: "this is a shirt", price: 999 },
+    { id: 3, name: "pants", description: "this is a pants", price: 999 },
+  ];
+  const [products, setProducts] = useState(list); // Instancia por default los productos dentro de la lista
+
   return (
-    <div>
+    <>
       <h1>Listado de productos</h1>
-      <strong>Titulo de una card</strong>
-      <a href="#">$9.999</a>
-      <Link to={"/product/idrandom"}>Ver producto</Link>
-    </div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <strong>{product.name}</strong>
+            <a href="#">${product.price}</a>
+            <Link to={`/product/${product.id}`}>Ver producto</Link>
+          </div>
+        );
+      })}
+    </>
   );
 };
