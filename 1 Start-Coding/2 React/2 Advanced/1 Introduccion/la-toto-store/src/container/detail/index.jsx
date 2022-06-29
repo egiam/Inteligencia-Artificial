be2c3e4cb0caa46ef94c9d6aa9f270d0a751edfa
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-06-29 20:43:52 -0300

    Update index.jsx

--- 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/detail/index.jsx
+++ 1 Start-Coding/2 React/2 Advanced/1 Introduccion/la-toto-store/src/container/detail/index.jsx
@@ -1,14 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/Cart";
+import { useEffect } from "react";
 
 export const DetailContainer = () => {
+  const [product, setProduct] = useState({});
   const { id } = useParams(); //Busca el parametro que se genera de ID en el URL y lo guarda en la variable id.
+  const { listProducts } = useContext(CartContext); //Usa el contexto de CartContext.
+
+  useEffect((id) => {
+    let result = listProducts.filter((product) => product.id === Number(id)); //Filtra el producto que se encuentra en la lista de productos.
+    setProduct(result[0]); //Guarda el producto en la variable product.
+  }, []);
 
   return (
     <>
       URL
-      <h1>Nombre del producto - {id}</h1>
-      <strong>Descripcion</strong>
-      <a href="#">$9.999</a>
+      <h1>
+        {product.name} - {id}
+      </h1>
+      <strong>No tengo descripcion</strong>
+      <a href="#">${product.price}</a>
       <button>Comprar</button>
     </>
   );
